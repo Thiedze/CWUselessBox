9c584e110338c19a48e49a90142afb713b813e6a
Sebastian Thiems <sthiems@sieber-riedl.de>
2020-09-03 16:59:58 +0200

    Add some small logic

--- main_c/main_c.ino
+++ main_c/main_c.ino
@@ -1,3 +1,21 @@
+/*
+
+ switchTop (left) = LOW
+ switchTop (right) = HIGH
+ 
+ switchButtom (pressed) = HIGH
+ switchButtom (released) = LOW
+ 
+ led (on) = HIGH
+ led (off) = LOW
+ 
+ Logic:
+ if switchTop == HIGH     then move arm up
+ if switchTop == LOW      then move arm down
+ if switchButtom == HIGH  then stop move arm
+ 
+ */
+
 int switchButtom = 2;            
 int switchTop = 3;
 int sensitivity = 200;
@@ -5,25 +23,56 @@ int sensitivity = 200;
 int led = 13;
 
 void setup() {
+
+  Serial.begin(9600);
+
   pinMode( switchButtom, INPUT_PULLUP);
   pinMode( switchTop, INPUT_PULLUP);
   pinMode( led, OUTPUT);
 }
 
+void blink_led(int blink_delay) {
+  for (int round = 0; round < 10; round++)  {
+    digitalWrite(led, HIGH);
+    delay(blink_delay);
+    digitalWrite(led, LOW);
+    delay(blink_delay);
+  }
+}
+
+void move_arm_up() {
+  blink_led(50);
+}
+
+void move_arm_down() {
+  blink_led(200);
+}
+
+void move_arm_stop() {
+  digitalWrite(led, LOW);   
+}
+
 void loop() {
 
-  if (digitalRead(switchButtom) == LOW) {
-    if (digitalRead(switchTop) == LOW) {
-      digitalWrite(led, HIGH);
-    } 
-    else {
-      digitalWrite(led, LOW); 
-    }
+  if (digitalRead(switchTop) == HIGH) { 
+    move_arm_up();
   } 
-  else {
-    digitalWrite(led, LOW); 
+  else if (digitalRead(switchButtom) == LOW && digitalRead(switchTop) == LOW) { 
+    move_arm_down();
+  } 
+  else  if (digitalRead(switchButtom) == HIGH) { 
+    move_arm_stop();
   }
 
   delay(sensitivity);
 }
+
+
+
+
+
+
+
+
+
 
